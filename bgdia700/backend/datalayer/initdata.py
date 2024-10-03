048d0150595c40414f7196a2c7b9fd0f0ad23d30
lolomasterIA <laury.magne@gmail.com>
2024-10-03 16:19:43 +0200

    encore

--- bgdia700/backend/datalayer/initdata.py
+++ bgdia700/backend/datalayer/initdata.py
@@ -32,7 +32,7 @@ class DataLayer:
             return pd.read_pickle(file)
 
     def load_data(self):
-        """Charge tous les fichiers de données dans les attributs appropriés"""
+        """Charge tous les fichiers de données"""
         # Charger les fichiers CSV
         self.interactions_test = self.load_csv(
             str(data_dir) + "/interactions_test.csv")
