66f6647f1994a980a3359785494af93ca935c6e8
lolomasterIA <laury.magne@gmail.com>
2024-10-05 16:55:20 +0200

    test pytest CI

--- tests/test_backend/test_datalayer/test_initdata.py
+++ tests/test_backend/test_datalayer/test_initdata.py
@@ -1,7 +1,7 @@
 from unittest import mock
 import pandas as pd
 import pytest
-from bgdia700.backend.datalayer.initdata import DataLayer, FileUnreadableError
+from initdata import DataLayer, FileUnreadableError
 
 
 @pytest.fixture
